e4624ed2558b82da1f0b96c68fa56a9c9bb0c0c2
Enrique <enrique.navarro@externo.ibermatica.com>
2021-07-09 08:32:48 +0200

    act

--- Product-Service/src/main/java/com/product/service/app/ProductServiceApplication.java
+++ Product-Service/src/main/java/com/product/service/app/ProductServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //@EnableEurekaClient
+//@EnableDiscoveryClient
 @EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan({"com.commons.service.app.entity"})
